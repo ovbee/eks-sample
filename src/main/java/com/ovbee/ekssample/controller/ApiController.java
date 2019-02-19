31e5ac444ccea9be83c78bb528d6b3d8c906c016
tamjenkins <businessrat@gmail.com>
2019-02-19 17:02:08 +0800

    bug fix

--- src/main/java/com/ovbee/ekssample/controller/ApiController.java
+++ src/main/java/com/ovbee/ekssample/controller/ApiController.java
@@ -4,12 +4,14 @@ import com.ovbee.ekssample.model.IpInfo;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
 
 @RestController
 @RequestMapping("/api")
@@ -17,7 +19,7 @@ public class ApiController {
     private static final Logger logger = LogManager.getLogger(ApiController.class);
 
     @RequestMapping(value = "/ipinfo", method = RequestMethod.GET)
-    public IpInfo ipInformation(Model model, HttpServletRequest request) {
+    public IpInfo ipInformation(HttpServletRequest request) {
         String remoteAddress = request.getRemoteAddr();
         String localAddress = request.getLocalAddr();
         String headerIp = request.getHeader("X-FORWARDED-FOR");
@@ -27,6 +29,20 @@ public class ApiController {
         IpInfo info = new IpInfo();
         info.setClientIp(remoteAddress);
         info.setRemoteIp(localAddress);
+
+        String ips = request.getHeader("X-Forwarded-For");
+        if (StringUtils.isEmpty(ips)) {
+            ips = request.getHeader("Proxy-Client-IP");
+        }
+        if (StringUtils.isEmpty(ips)) {
+            ips = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (StringUtils.isEmpty(ips)) {
+            ips = request.getRemoteAddr();
+        }
+        String ip = Arrays.stream(ips.split(",")).findFirst().get();
+        logger.info("---ips---" + ips);
+        info.setClientIp(remoteAddress + "#" + ips);
         return info;
     }
 }
