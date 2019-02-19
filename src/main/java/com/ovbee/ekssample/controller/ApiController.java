35be7ecc012762a57b858d49e91ea05aaf7efde1
tamjenkins <businessrat@gmail.com>
2019-02-19 15:57:52 +0800

    bug fix

--- src/main/java/com/ovbee/ekssample/controller/ApiController.java
+++ src/main/java/com/ovbee/ekssample/controller/ApiController.java
@@ -1,26 +1,29 @@
 package com.ovbee.ekssample.controller;
 
+import com.ovbee.ekssample.model.IpInfo;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
-@Controller
-public class HomeController {
-    private static final Logger logger = LogManager.getLogger(HomeController.class);
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+    private static final Logger logger = LogManager.getLogger(ApiController.class);
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String home(Model model, HttpServletRequest request) {
+    @RequestMapping(value = "/ipinfo", method = RequestMethod.GET)
+    public IpInfo ipInformation(Model model, HttpServletRequest request) {
         String remoteAddress = request.getRemoteAddr();
         String localAddress = request.getLocalAddr();
         logger.info("---remoteAddress---" + remoteAddress);
         logger.info("---localAddress---" + localAddress);
-        model.addAttribute("clientIp", remoteAddress);
-        model.addAttribute("remoteIp", localAddress);
-        return "/home";
+        IpInfo info = new IpInfo();
+        info.setClientIp(remoteAddress);
+        info.setRemoteIp(localAddress);
+        return info;
     }
 }
---
+++ src/main/java/com/ovbee/ekssample/model/IpInfo.java
@@ -0,0 +1,22 @@
+package com.ovbee.ekssample.model;
+
+public class IpInfo {
+    private String remoteIp;
+    private String clientIp;
+
+    public String getRemoteIp() {
+        return remoteIp;
+    }
+
+    public void setRemoteIp(String remoteIp) {
+        this.remoteIp = remoteIp;
+    }
+
+    public String getClientIp() {
+        return clientIp;
+    }
+
+    public void setClientIp(String clientIp) {
+        this.clientIp = clientIp;
+    }
+}
