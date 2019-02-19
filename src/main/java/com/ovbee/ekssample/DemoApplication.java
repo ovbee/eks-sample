564bf055238f8dafe414a3072386d62d05923972
tamjenkins <businessrat@gmail.com>
2019-02-19 14:18:18 +0800

    daily update

--- src/main/java/com/ovbee/ekssample/DemoApplication.java
+++ src/main/java/com/ovbee/ekssample/DemoApplication.java
@@ -1,7 +1,13 @@
 package com.ovbee.ekssample;
 
+import nz.net.ultraq.thymeleaf.LayoutDialect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -9,5 +15,14 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
+	@Bean
+	public LayoutDialect layoutDialect() {
+		return new LayoutDialect();
+	}
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver slr = new SessionLocaleResolver();
+		slr.setDefaultLocale(Locale.CHINA); // Set default Locale as US
+		return slr;
+	}
 }
--- src/main/java/com/ovbee/ekssample/controller/HomeController.java
+++ src/main/java/com/ovbee/ekssample/controller/HomeController.java
@@ -1,4 +1,36 @@
 package com.ovbee.ekssample.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@Controller
 public class HomeController {
+    private static final Logger logger = LogManager.getLogger(HomeController.class);
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login(Model model, String error, String logout) {
+        if (error != null)
+            model.addAttribute("errorMsg", "Your username and password is invalid.");
+
+        if (logout != null)
+            model.addAttribute("message", "You have been logged out successfully.");
+
+        return "login";
+    }
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home(Model model, HttpServletRequest request) {
+        String remoteAddress = request.getRemoteAddr();
+        String localAddress = request.getLocalAddr();
+        logger.info("---remoteAddress---" + remoteAddress);
+        logger.info("---localAddress---" + localAddress);
+        model.addAttribute("clientIp", remoteAddress);
+        model.addAttribute("remoteIp", localAddress);
+        return "/home";
+    }
 }
